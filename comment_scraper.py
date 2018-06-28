476fde4f1f7cf8741621507720b770d2f14f6201
Bryson Seiler <Bryson.Seiler@gmail.com>
2018-06-28 13:35:02 -0600

    Added comment scraper

--- comment_scraper.py
+++ comment_scraper.py
@@ -1,4 +1,7 @@
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
+import re
 
 
 #Subreddit object:
@@ -7,3 +10,41 @@
     #submission titles
     #number of comments gathered
     #comments
+
+class Subreddit:
+
+    def __init__(self, name, num_submissions, submission_list, num_comments, comment_list):
+        self.name = name
+        self.num_submissions = num_submissions
+        self.submission_list = submission_list
+        self.num_comments = num_comments
+        self.comment_list = comment_list
+
+
+def scrape(reddit_bot, subreddit_names, num_submissions, num_comments, tree_depth):
+
+    #Set stop words to english
+    stop_words = set(stopwords.words('english'))
+
+    for subreddit_name in subreddit_names:
+
+        #Get subreddit object
+        subreddit = get_subreddit(reddit_bot, subreddit_name)
+
+        print("Gathering %d comments from %d submissions in %s..." % (num_comments, num_submissions, subreddit_name))
+
+        for submission in subreddit.top(limit = num_submissions):
+            print(submission.title)
+
+
+
+
+
+
+
+
+
+
+def get_subreddit(reddit_bot, subreddit_name):
+    subreddit_obj = reddit_bot.subreddit(subreddit_name)
+    return subreddit_obj
--- main_script.py
+++ main_script.py
@@ -1,12 +1,25 @@
 import config
 import praw
 
-
+import comment_scraper as cs
 
 
 def main():
 
+    #Bot login
     reddit_bot = login()
+
+    subreddit_names = list(str(input("Enter the names of the subreddits (separated by commas) that you would like to analyze: ")).split(","))
+
+    #Values can be changed to collect more/less data
+    num_submissions = 1
+    num_comments = 1
+    tree_depth = 1
+
+    #Scrape comments from subreddits
+    cs.scrape(reddit_bot, subreddit_names, num_submissions, num_comments, tree_depth)
+
+
 
 
 def login():
@@ -16,6 +29,8 @@ def login():
                         client_id = config.client_id,
                         client_secret = config.client_secret,
                         user_agent = config.user_agent)
+
+    return reddit_bot
 
 
 if __name__ == '__main__':
