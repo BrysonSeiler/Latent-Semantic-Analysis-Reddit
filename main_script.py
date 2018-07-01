82f24375e35316874d55a557ea66df7d89692300
Bryson Seiler <Bryson.Seiler@gmail.com>
2018-06-30 22:55:37 -0600

    Removed comment scraper since submissions titles work as much better predictors

--- main_script.py
+++ main_script.py
@@ -1,7 +1,6 @@
 import config
 import praw
 
-import comment_scraper as cs
 import submission_scraper as ss
 import semantic_analysis as sa
 
@@ -13,59 +12,31 @@ def main():
     #Get user input for which subreddits to analyze
     subreddit_names = list(str(input("Enter the names of the subreddits (separated by commas) that you would like to analyze: ")).split(","))
 
-    print('''Text types:
-                    1. Submission titles
-                    2. Comments \n
-          ''')
 
-    text_type = int(input("Enter type of text you would like to analyze (integer): "))
+    print("Analyzing submission titles...\n")
 
-    if text_type == 1:
-        print("Analyzing submission titles...\n")
+    #Values can be changed to collect more/less data
+    num_submissions = 1500
 
-        #Values can be changed to collect more/less data
-        num_submissions = 100
+    #Scrape submission titles from chosen subreddits
+    subreddit_objects = ss.scrape(reddit_bot, subreddit_names, num_submissions)
 
-        #Scrape submission titles from chosen subreddits
-        subreddit_objects = ss.scrape(reddit_bot, subreddit_names, num_submissions)
+    for obj in subreddit_objects:
+        print("Subreddit name: ", obj.name, "\n")
+        print("Number of submissions: ", obj.num_submissions, "\n")
+        #print("Submissions: ", obj.submission_list, "\n")
+        print("Number of submissions read: ", obj.num_submissions_read, "\n \n")
 
-        for obj in subreddit_objects:
-            print("Subreddit name: ", obj.name, "\n")
-            print("Number of submissions: ", obj.num_submissions, "\n")
-            #print("Submissions: ", obj.submission_list, "\n")
-            print("Number of submissions read: ", obj.num_submissions_read, "\n \n")
+    #Bundle together all submission titles from all subreddits
+    submissions = ss.bundle_submissions(subreddit_objects)
 
-        #Bundle together all submission titles from all subreddits
-        submissions = ss.bundle_submissions(subreddit_objects)
+    #Tag each comment based off of which subreddit it came from
+    subreddit_tags, numeric_tags = ss.get_tags(subreddit_objects)
 
-        #Tag each comment based off of which subreddit it came from
-        subreddit_tags, numeric_tags = ss.get_tags(subreddit_objects)
+    #Learn comment vocabulary
+    #num_strings, text, subreddit_tags, numeric_tags
+    sa.run_lsa(len(submissions), submissions, subreddit_tags, numeric_tags)
 
-        #Learn comment vocabulary
-        #num_strings, text, subreddit_tags, numeric_tags
-        sa.run_lsa(len(submissions), submissions, subreddit_tags, numeric_tags)
-
-
-    if text_type == 2:
-        print("Analyzing comments...\n")
-        
-        #Values can be changed to collect more/less data
-        num_submissions = 5
-        num_comments = 10
-        tree_depth = 1
-
-        #Scrape comments from chosen subreddits
-        subreddit_objects = cs.scrape(reddit_bot, subreddit_names, num_submissions, num_comments, tree_depth)
-    
-        #Bundle together all comments from all subreddits
-        comments = cs.bundle_comments(subreddit_objects)
-
-        #Tag each comment based off of which subreddit it came from
-        subreddit_tags, numeric_tags = cs.get_tags(subreddit_objects)
-
-        #Learn comment vocabulary
-        sa.run_lsa(len(comments), comments, subreddit_tags, numeric_tags)
-    
 
 def login():
 
